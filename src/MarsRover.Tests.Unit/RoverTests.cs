cf7f69ab129d65982ed5c059714b4be61689fda5
Marco Bettiolo <marco@bettiolo.it>
2014-03-31 23:59:44 +0100

    Added first test for Planet and Rover Referenced NUnit and FluentAssertions

---
+++ src/MarsRover.Tests.Unit/RoverTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace MarsRover.Tests.Unit
+{
+	[TestFixture]
+	public class RoverTests
+	{
+
+		[Test]
+		public void RoverLandsOnPlanetWithDefaultPosition()
+		{
+			var planet = new Planet();
+			var rover = new Rover(planet);
+
+			rover.Land();
+
+			rover.Position.X.Should().Be(0);
+			rover.Position.Y.Should().Be(0);
+		}
+	}
+
+	public class Rover
+	{
+
+		private readonly Planet _planet;
+
+		public Rover(Planet planet)
+		{
+			_planet = planet;
+		}
+
+		public Point Position { get; set; }
+
+		public void Land()
+		{
+			
+		}
+
+	}
+
+	public class Planet
+	{
+
+	}
+
+}
